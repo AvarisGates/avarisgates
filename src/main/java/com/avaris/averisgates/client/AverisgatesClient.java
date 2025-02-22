8cdb55ecd504128bed623cbfc60f753288a57681
bejker123 <1piotrkucharski@gmail.com>
2025-02-23 00:20:16 +0100

    feat: add ability keybinds

--- src/main/java/com/avaris/averisgates/client/AverisgatesClient.java
+++ src/main/java/com/avaris/averisgates/client/AverisgatesClient.java
@@ -13,23 +13,39 @@ import org.lwjgl.glfw.GLFW;
 
 public class AverisgatesClient implements ClientModInitializer {
 
+        private static final AbilityKeyBind ABILITY_0_KEY_BIND = new AbilityKeyBind(
+                KeyBindingHelper.registerKeyBinding(new KeyBinding(
+    "key.averisgates.ability_0", // The translation key of the keybinding's name
+                InputUtil.Type.KEYSYM, // The type of the keybinding, KEYSYM for keyboard, MOUSE for mouse.
+                GLFW.GLFW_KEY_R, // The keycode of the key
+                "category.averisgates.abilities" // The translation key of the keybinding's category.
+                )));
+
     private static final AbilityKeyBind ABILITY_1_KEY_BIND = new AbilityKeyBind(
             KeyBindingHelper.registerKeyBinding(new KeyBinding(
-"key.averisgates.ability_1", // The translation key of the keybinding's name
-            InputUtil.Type.KEYSYM, // The type of the keybinding, KEYSYM for keyboard, MOUSE for mouse.
-            GLFW.GLFW_KEY_R, // The keycode of the key
-            "category.averisgates.abilities" // The translation key of the keybinding's category.
+                    "key.averisgates.ability_1", // The translation key of the keybinding's name
+                    InputUtil.Type.KEYSYM, // The type of the keybinding, KEYSYM for keyboard, MOUSE for mouse.
+                    GLFW.GLFW_KEY_X, // The keycode of the key
+                    "category.averisgates.abilities" // The translation key of the keybinding's category.
+            )));
+
+    private static final AbilityKeyBind ABILITY_2_KEY_BIND = new AbilityKeyBind(
+            KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                    "key.averisgates.ability_2", // The translation key of the keybinding's name
+                    InputUtil.Type.KEYSYM, // The type of the keybinding, KEYSYM for keyboard, MOUSE for mouse.
+                    GLFW.GLFW_KEY_C, // The keycode of the key
+                    "category.averisgates.abilities" // The translation key of the keybinding's category.
             )));
 
     @Override
     public void onInitializeClient() {
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            boolean wasPressed = ABILITY_1_KEY_BIND.getKeyBind().wasPressed();
-            int cooldown = ABILITY_1_KEY_BIND.tickCooldown();
+            boolean wasPressed = ABILITY_0_KEY_BIND.getKeyBind().wasPressed();
+            int cooldown = ABILITY_0_KEY_BIND.tickCooldown();
             if(cooldown >= 0 && wasPressed){
-                client.player.sendMessage(Text.literal("Ability on cooldown for ").append(Text.literal(String.valueOf(ABILITY_1_KEY_BIND.cooldown))),true);
+                client.player.sendMessage(Text.literal("Ability on cooldown for ").append(Text.literal(String.valueOf(ABILITY_0_KEY_BIND.cooldown))),true);
             }else if(wasPressed){
-                ABILITY_1_KEY_BIND.cooldown = 20 * 5;
+                ABILITY_0_KEY_BIND.cooldown = 20 * 5;
                 client.player.sendMessage(Text.literal("Client sent ability one"), false);
                 ClientPlayNetworking.send(new CastPlayerClassAbilityC2S(PlayerClassAbility.PlayerClassAbilityType.Swing));
             }
