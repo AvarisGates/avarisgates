1f914e0195dea48f13faf49c0834650b1034a17d
bejker123 <1piotrkucharski@gmail.com>
2025-02-22 20:08:43 +0100

    chore: add project template

---
+++ src/main/java/com/avaris/averisgates/Averisgates.java
@@ -0,0 +1,10 @@
+package com.avaris.averisgates;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Averisgates implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/com/avaris/averisgates/client/AverisgatesClient.java
@@ -0,0 +1,10 @@
+package com.avaris.averisgates.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AverisgatesClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
