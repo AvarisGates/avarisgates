7a8fe9ee7e37da23ce90790f2c65416407e4d8a3
bejker123 <1piotrkucharski@gmail.com>
2025-02-23 21:00:55 +0100

    feat: inventory rendering

--- src/main/java/com/avaris/averisgates/client/HudRenderer.java
+++ src/main/java/com/avaris/averisgates/client/HudRenderer.java
@@ -11,15 +11,15 @@ public class HudRenderer {
     public static void renderExperience(DrawContext context, RenderTickCounter tickCounter) {
         String expString = String.valueOf(AverisgatesClient.getLevel());
         int x = (context.getScaledWindowWidth() - MinecraftClient.getInstance().textRenderer.getWidth(expString)) / 2;
-        int y = 13;
+        int y = 8;
         context.drawText(MinecraftClient.getInstance().textRenderer, Text.literal(expString),x, y, Colors.GREEN,true);
         y = 1;
         int screenWidth = context.getScaledWindowWidth() / 2;
         int width = 200;
 
-        context.fill(screenWidth - width / 2 + 1,y + 1,screenWidth + width / 2 - 1,y + 10,Colors.BLACK);
+        context.fill(screenWidth - width / 2 + 1,y + 1,screenWidth + width / 2 - 1,y + 5,Colors.BLACK);
 
-        context.fill(screenWidth - width / 2 + 2,y + 2,screenWidth + width / 2 - 2,y + 9,Colors.YELLOW);
+        context.fill(screenWidth - width / 2 + 2,y + 2,screenWidth + width / 2 - 2,y + 4,Colors.YELLOW);
     }
 
     public static void renderHealthBar(DrawContext context, PlayerEntity player, int x, int y, int lines, int regeneratingHeartIndex, float maxHealth, int lastHealth, int health, int absorption, boolean blinking) {
