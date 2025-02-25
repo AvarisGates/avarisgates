c0a2921235535441e1b407208b5fa53cb76bf300
bejker123 <1piotrkucharski@gmail.com>
2025-02-25 11:17:07 +0100

    ref: create a AbililityEntity class

---
+++ src/main/java/com/avaris/avarisgates/core/entity/AbilityEntity.java
@@ -0,0 +1,76 @@
+package com.avaris.avarisgates.core.entity;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.data.DataTracker;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.world.World;
+
+import java.util.UUID;
+
+public class AbilityEntity extends Entity{
+    protected UUID ownerUUID;
+    protected float damage;
+    protected int tickLeft; //Ticks left until despawn
+
+    public AbilityEntity(EntityType<?> type, World world) {
+        super(type, world);
+    }
+
+    @Override
+    protected void initDataTracker(DataTracker.Builder builder) {
+    }
+
+    @Override
+    public boolean damage(ServerWorld world, DamageSource source, float amount) {
+        return false;
+    }
+
+    @Override
+    protected void readCustomDataFromNbt(NbtCompound nbt) {
+        this.ownerUUID = nbt.getUuid("owner");
+        this.damage = nbt.getFloat("damage");
+        this.tickLeft = nbt.getInt("ticks_left");
+    }
+
+    @Override
+    protected void writeCustomDataToNbt(NbtCompound nbt) {
+        nbt.putUuid("owner",this.ownerUUID);
+        nbt.putFloat("damage",this.damage);
+        nbt.putInt("ticks_left",this.tickLeft);
+    }
+
+    @Override
+    public void tick() {
+        if(this.tickLeft-- < 0){
+            this.remove(RemovalReason.DISCARDED);
+            return;
+        }
+        super.tick();
+    }
+
+    public void setDamage(float damage) {
+        this.damage = damage;
+    }
+
+    public float getDamage() {
+        return damage;
+    }
+
+    public UUID getOwnerUUID() {
+        return ownerUUID;
+    }
+
+    public void setOwnerUuid(UUID ownerUuid) {
+        this.ownerUUID = ownerUuid;
+    }
+    public int getTickLeft() {
+        return tickLeft;
+    }
+
+    public void setTickLeft(int tickLeft) {
+        this.tickLeft = tickLeft;
+        }
+}
