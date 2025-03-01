bb4f8fa2d4147b402d53154f9793364b17817f2d
bejker123 <1piotrkucharski@gmail.com>
2025-03-01 03:03:48 +0100

    fix: restore deleted mana sync code

--- src/main/java/com/avaris/avarisgates/core/player/ManaAttachment.java
+++ src/main/java/com/avaris/avarisgates/core/player/ManaAttachment.java
@@ -114,6 +114,11 @@ public class ManaAttachment extends PlayerResource{
     }
 
     public static void initForPlayer(ServerPlayerEntity player) {
-
+        ManaAttachment manaAttachment = ManaAttachment.getMana(player);
+        if(manaAttachment == null||manaAttachment.maxValue == 0){
+            manaAttachment = new ManaAttachment(100,100);
+        }
+        //Sync mana with the client
+        ManaAttachment.setMana(player,manaAttachment,true);
     }
 }
