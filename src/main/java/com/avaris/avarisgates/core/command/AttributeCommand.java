385fca1640f153b789a4032a3da7718244bc570b
bejker123 <1piotrkucharski@gmail.com>
2025-03-14 19:13:17 +0100

    ref: cleanup AttributeCommand; feat: /attributes reset command

--- src/main/java/com/avaris/avarisgates/core/command/AttributeCommand.java
+++ src/main/java/com/avaris/avarisgates/core/command/AttributeCommand.java
@@ -2,7 +2,9 @@ package com.avaris.avarisgates.core.command;
 
 import com.avaris.avarisgates.core.player.attribute.Attribute;
 import com.avaris.avarisgates.core.player.attribute.AttributeType;
+import com.mojang.brigadier.context.CommandContext;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
 
@@ -17,30 +19,76 @@ public class AttributeCommand {
         CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> dispatcher.register(
                 literal("attributes")
                         .requires(x -> x.hasPermissionLevel(2))
-                        .then(literal("get").executes(context -> {
-                            ServerPlayerEntity player = context.getSource().getPlayer();
-                            if(player == null){
-                                return 1;
-                            }
-                            for(AttributeType type : AttributeType.values()){
-                                Attribute attribute = Attribute.getAttribute(player,type);
-                                player.sendMessage(Text.literal(attribute.getType() + ": "+attribute.getValue()));
-                            }
-                           return 0;
-                        }))
-                        .then(literal("set").then(argument("attribute",new AttributeArgumentType()).then(argument("value",integer(0)).executes(context ->{
-                            AttributeType type = context.getArgument("attribute",AttributeType.class);
-                            long value = context.getArgument("value",Integer.class);
-                            ServerPlayerEntity player = context.getSource().getPlayer();
-                            if(player == null){
-                                return 1;
-                            }
-                            Attribute attribute = new Attribute(type,value);
-                            Attribute.setAttribute(player,attribute);
-                            player.sendMessage(Text.literal(attribute.getType() + " set: "+attribute.getValue()));
-                            return 0;
-                        }))))
-
+                        .then(literal("get").executes(AttributeCommand::getAttributes)
+                                .then(argument("attribute", new AttributeArgumentType())
+                                        .executes(AttributeCommand::getAttribute)))
+                        .then(literal("set").then(argument("attribute", new AttributeArgumentType())
+                                .then(argument("value", integer(0))
+                                        .executes(AttributeCommand::setAttribute))))
+                        .then(literal("reset").executes(AttributeCommand::resetAttributes)
+                                .then(argument("attribute", new AttributeArgumentType())
+                                        .executes(AttributeCommand::resetAttribute)))
         ));
+    }
+
+    private static int resetAttribute(CommandContext<ServerCommandSource> context) {
+        AttributeType type = context.getArgument("attribute", AttributeType.class);
+        ServerPlayerEntity player = context.getSource().getPlayer();
+        if (player == null) {
+            return 1;
+        }
+        Attribute attribute = Attribute.resetAttribute(player, type);
+        player.sendMessage(Text.literal("Resetting " + type + "..."));
+        player.sendMessage(Text.literal(type + ": " + attribute.getValue()));
+        return 0;
+    }
+
+    private static int resetAttributes(CommandContext<ServerCommandSource> context) {
+        ServerPlayerEntity player = context.getSource().getPlayer();
+        if (player == null) {
+            return 1;
+        }
+        player.sendMessage(Text.literal("Resetting attributes..."));
+        for (AttributeType type : AttributeType.values()) {
+            Attribute attribute = Attribute.resetAttribute(player, type);
+            player.sendMessage(Text.literal(type + ": " + attribute.getValue()));
+        }
+        return 0;
+    }
+
+    private static int getAttribute(CommandContext<ServerCommandSource> context) {
+        AttributeType type = context.getArgument("attribute", AttributeType.class);
+        ServerPlayerEntity player = context.getSource().getPlayer();
+        if (player == null) {
+            return 1;
+        }
+        Attribute attribute = Attribute.getAttribute(player, type);
+        player.sendMessage(Text.literal(attribute.getType() + ": " + attribute.getValue()));
+        return 0;
+    }
+
+    private static int getAttributes(CommandContext<ServerCommandSource> context) {
+        ServerPlayerEntity player = context.getSource().getPlayer();
+        if (player == null) {
+            return 1;
+        }
+        for (AttributeType type : AttributeType.values()) {
+            Attribute attribute = Attribute.getAttribute(player, type);
+            player.sendMessage(Text.literal(attribute.getType() + ": " + attribute.getValue()));
+        }
+        return 0;
+    }
+
+    private static int setAttribute(CommandContext<ServerCommandSource> context) {
+        AttributeType type = context.getArgument("attribute", AttributeType.class);
+        long value = context.getArgument("value", Integer.class);
+        ServerPlayerEntity player = context.getSource().getPlayer();
+        if (player == null) {
+            return 1;
+        }
+        Attribute attribute = new Attribute(type, value);
+        Attribute.setAttribute(player, attribute);
+        player.sendMessage(Text.literal(attribute.getType() + " set: " + attribute.getValue()));
+        return 0;
     }
 }
