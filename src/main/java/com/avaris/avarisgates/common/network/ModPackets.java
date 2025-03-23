6134b8c999a87270f50f123cd8e242b5c87e87d6
bejker123 <1piotrkucharski@gmail.com>
2025-03-23 16:02:36 +0100

    docs: ref ModPacket docs

--- src/main/java/com/avaris/avarisgates/common/network/ModPackets.java
+++ src/main/java/com/avaris/avarisgates/common/network/ModPackets.java
@@ -4,17 +4,19 @@ import com.avaris.avarisgates.common.player.PlayerManager;
 import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 
-// This class handles registering the mod packets
-// Packet Description:
-// Both packet types are required to be registered on the client and the server.
-// C2S - Client to Server, a packet sent by the client, received and handled by the server.
-// DON'T TRUST THE CLIENT WITH ANYTHING IMPORTANT
-// Remember the client can send any data, we have to validate it and issue an adequate response.
-// S2C - Server to Client, a packet sent by the server, received and handled by the client.
-// We don't need to validate these packets.
-// If data isn't synced properly we always prioritize the server's version of the data.
-// If something isn't ABSOLUTELY required to be sent to the client we don't.
-// This conserves both computing power on the server and packets sent to the client.
+/**
+ * This class handles registering the mod packets.<br>
+ * Packet Description:<br>
+ * Both packet types are required to be registered on the client and the server.<br>
+ * C2S - Client to Server, a packet sent by the client, received and handled by the server.<br>
+ * DON'T TRUST THE CLIENT WITH ANYTHING IMPORTANT<br>
+ * Remember the client can send any data, we have to validate it and issue an adequate response.<br>
+ * S2C - Server to Client, a packet sent by the server, received and handled by the client.<br>
+ * We don't need to validate these packets.<br>
+ * If data isn't synced properly we always prioritize the server's version of the data.<br>
+ * If something isn't ABSOLUTELY required to be sent to the client we don't.<br>
+ * This conserves both computing power on the server and packets sent to the client.
+ */
 public class ModPackets {
 
     // A common (server and client side) function to register the mod packets
