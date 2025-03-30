ebdda979446cdd92d3d19ae70fb10092aed670ef
bejker123 <1piotrkucharski@gmail.com>
2025-03-30 14:12:31 +0200

    ref(mixin): remove debug code

--- src/main/java/com/avaris/avarisgates/mixin/client/InGameHudMixin.java
+++ src/main/java/com/avaris/avarisgates/mixin/client/InGameHudMixin.java
@@ -44,46 +44,6 @@ public abstract class InGameHudMixin {
                 absorption,
                 renderDecreasedHealth
         );
-        //InGameHud.HeartType heartType = InGameHud.HeartType.fromPlayerState(player);
-        //boolean bl = player.getWorld().getLevelProperties().isHardcore();
-        //int i = MathHelper.ceil((double)maxHealth / 2.0);
-        //int j = MathHelper.ceil((double)absorption / 2.0);
-        //int k = i * 2;
-
-        //for (int l = i + j - 1; l >= 0; l--) {
-        //    int m = l / 10;
-        //    int n = l % 10;
-        //    int o = x + n * 8;
-        //    int p = y - m * lines;
-        //    if (lastHealth + absorption <= 4) {
-        //        p += this.random.nextInt(2);
-        //    }
-
-        //    if (l < i && l == regeneratingHeartIndex) {
-        //        p -= 2;
-        //    }
-
-        //    this.drawHeart(context, InGameHud.HeartType.CONTAINER, o, p, bl, blinking, false);
-        //    int q = l * 2;
-        //    boolean bl2 = l >= i;
-        //    if (bl2) {
-        //        int r = q - k;
-        //        if (r < absorption) {
-        //            boolean bl3 = r + 1 == absorption;
-        //            this.drawHeart(context, heartType == InGameHud.HeartType.WITHERED ? heartType : InGameHud.HeartType.ABSORBING, o, p, bl, false, bl3);
-        //        }
-        //    }
-
-        //    if (blinking && q < health) {
-        //        boolean bl4 = q + 1 == health;
-        //        this.drawHeart(context, heartType, o, p, bl, true, bl4);
-        //    }
-
-        //    if (q < lastHealth) {
-        //        boolean bl4 = q + 1 == lastHealth;
-        //        this.drawHeart(context, heartType, o, p, bl, false, bl4);
-        //    }
-        //}
     }
 
     @Inject(method = "shouldRenderExperience",at = @At("HEAD"),cancellable = true)
